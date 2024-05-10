aca20d7683dd54a7b7887f6edd1f03285ea5d046
David Alejandro Marin Alzate <jandro240@gmail.com>
2024-05-09 21:45:49 -0500

    roles

---
+++ src/core/models/role/role.ts
@@ -0,0 +1,11 @@
+export interface Role {
+    id: number;
+    idDepartment: string;
+    name: string;
+    description: string;
+    idClient: string;
+    creationUserId: string;
+    creationDate: string;
+    modificationUser: string;
+    modificationDate: string;
+  }
